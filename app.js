16b0604a14225228efe7ccd93bd7fb2dea34f89b
invegas <juststayinvegas@gmail.com>
2013-12-12 23:35:05 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
+var FeedParser = require('feedparser');
+var request = require('request');
+var parseString = require('xml2js').parseString;
+var fs = require('fs');
+
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
-app.listen(3000);
+// request('http://www.huxiu.com/rss/0.xml', function (error, response, body) {
+//     parseString(body, function (err, result) {
+//         console.dir(result);
+//     });
+// });
+
+fs.readFile(__dirname + '/foo.xml', function(err, data) {
+    parseString(data, function (err, result) {
+        console.dir(result.person);
+    });
+});
+
+
+app.listen(8000);
