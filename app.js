e74950082b5853e5007730c72da1beac7ab05242
invegas <juststayinvegas@gmail.com>
2013-12-15 00:39:36 +0800

    update

--- app.js
+++ app.js
@@ -5,22 +5,11 @@ var path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
 
 app.get("/test", function (req, res) {
-    FeedParser.parse("http://www.huxiu.com/rss/0.xml", function (data) {
+    var url = "http://127.0.0.1:8000/0.xml";
+    FeedParser.parse(url, function (data) {
         res.send(data);
     });
 })
--- modules/feedparser.js
+++ modules/feedparser.js
@@ -3,6 +3,15 @@
 var parseString = require('xml2js').parseString;
 var request = require('request');
 
+var arrayToObject = function (obj) {
+	for (var prop in obj) {
+		var val = obj[prop];
+		if (val.length && val.length === 1){
+			obj[prop]= val[0];
+		}
+	}
+}
+
 var _parse = function (data) {
 	var arr = [];
 
@@ -16,6 +25,7 @@ var _parse = function (data) {
 
 exports.parse = function (rssAddress, fn) {
 	var result = null;
+
 	request(rssAddress, function (error, response, body) {
 		if (error) {
 			fn(result);
