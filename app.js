b5fc27f42d28a995a03135947907dbd39444be65
invegas <juststayinvegas@gmail.com>
2013-12-16 22:54:34 +0800

    update

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var url = "http://www.slbook1996.com/book/allbang.htm";
 request(url, function (error, response, body) {
+    // $("#main .topbook:nth-of-type(6)")
+    // 找腾旭敏如何直接选中
     DomParser.parse(body, ".topbook ul li", {
         link: {
             selector: "a",
