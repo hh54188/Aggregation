d05a4a927417a9163f52a69668286daf44232c57
hh54188 <252438823@qq.com>
2013-12-18 19:34:19 +0800

    update

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 var express = require('express');
 var FeedParser = require('./modules/feedparser');
 var DomParser = require('./modules/domparser');
+var requestPages = require("./modules/requestPages");
 var path = require('path');
 var request = require('request');
 var cheerio = require('cheerio');
-var requestPages = require("./modules/requestPages");
+
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-requestPages.parse([
-	"http://www.billboard.com/charts/hot-100?page=0",
-	"http://www.billboard.com/charts/hot-100?page=1",
-	"http://www.billboard.com/charts/hot-100?page=2",
-	"http://www.billboard.com/charts/hot-100?page=3"
-], 10, "#block-system-main article", {
-	"id": {
-		attr: "id"
-	},
-	"cover_src": {
-		selector: "header .img-wrap img",
-		attr: "src"
-	}
-}, function (data) {
-	console.log(data.length);
+// requestPages.parse([
+// 	"http://www.billboard.com/charts/hot-100?page=0",
+// 	"http://www.billboard.com/charts/hot-100?page=1",
+// 	"http://www.billboard.com/charts/hot-100?page=2",
+// 	"http://www.billboard.com/charts/hot-100?page=3"
+// ], 10, "#block-system-main article", {
+// 	"id": {
+// 		attr: "id"
+// 	},
+// 	"cover_src": {
+// 		selector: "header .img-wrap img",
+// 		attr: "src"
+// 	}
+// }, function (data) {
+// 	console.log(data.length);
+// });
+
+
+FeedParser.parse("http://www.boxofficemojo.com/data/rss.php?file=topstories.xml", function (data) {
+	console.log(data);
 })
 
 
