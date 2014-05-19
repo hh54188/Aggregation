2f787c44fc7669f733ef40187f35837d9c6afa9e
hh54188 <252438823@qq.com>
2014-05-19 17:50:39 +0800

    update

---
+++ modules/pageHelper.js
@@ -0,0 +1,28 @@
+var dateFormat = function (date) {
+
+	var curTime = +new Date(); 
+	var suffix = "前";
+
+	var minutes = (curTime - date) / (60 * 1000);
+	var hours = (curTime - date) / (60 * 60 * 1000);
+	var days = Math.floor(hours / 24);
+
+	if (minutes < 60) {
+		return minutes + "分钟" + suffix;
+	} else if (hours < 24) {
+		return hours + "小时" + suffix;
+	} else if (days >= 1 && days <= 7) {
+		return days + "天" + suffix;
+	} else if (days > 7) {
+		return "一周" + suffix;
+	}	
+}
+
+var oddOrEvenRow = function (number) {
+	return number % 2 == 0? "even": "odd";
+}
+
+module.exports = {
+	dateFormat: dateFormat,
+	isOdd: oddOrEvenRow
+}
--- server.js
+++ server.js
@@ -3,42 +3,29 @@
 	http://scotch.io/tutorials/javascript/build-a-restful-api-using-node-and-express-4
 */
 
+// Globe Module
 var express = require("express");
 var ejs = require("ejs");
 
+// Config & Test data:
 var SampleData = require("./config/data");
 var config = require("./config/config");
 
+// Custom Module:
+var helper = require("./modules/pageHelper");
 
-var app = express();
 
+// Server Config:
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-var dateFormat = function (date) {
 
-	var curTime = +new Date(); 
-	var suffix = "前";
-
-	var minutes = (curTime - date) / (60 * 1000);
-	var hours = (curTime - date) / (60 * 60 * 1000);
-	var days = Math.floor(hours / 24);
-
-	if (minutes < 60) {
-		return minutes + "分钟" + suffix;
-	} else if (hours < 24) {
-		return hours + "小时" + suffix;
-	} else if (days >= 1 && days <= 7) {
-		return days + "天" + suffix;
-	} else if (days > 7) {
-		return "一周" + suffix;
-	}	
-}
-
+// Route Config
 app.get("/", function (req, res) {
 	res.render("index", {
 		list: SampleData,
-		dateFormat: dateFormat
+		config: config,
+		helper: helper
 	});
 });
 
@@ -46,4 +33,6 @@ app.get("/news", function (req, res) {
 
 });
 
+// Boot:
+var app = express();
 app.listen(8000);
